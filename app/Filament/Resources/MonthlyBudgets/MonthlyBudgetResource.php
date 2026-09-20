c049ecab84f61fbcbeb434fdd89c3de278e31820
Dermawanpurba <dermawanpurba@users.noreply.github.com>
2026-09-20 21:14:08 +0800

    fix(filament): resolve MonthlyBudgetResource navigationGroup property type mismatch for PHP 8.4

--- app/Filament/Resources/MonthlyBudgets/MonthlyBudgetResource.php
+++ app/Filament/Resources/MonthlyBudgets/MonthlyBudgetResource.php
@@ -18,15 +18,17 @@ use Filament\Tables\Table;
 
 class MonthlyBudgetResource extends Resource
 {
+    protected static ?string $model = MonthlyBudget::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+
     protected static ?string $navigationLabel = 'Maintenance Budget & Realization';
 
-    protected static ?string $navigationGroup = 'Planning & Coordination';
+    protected static string|\UnitEnum|null $navigationGroup = 'Planning & Coordination';
 
     protected static ?string $modelLabel = 'Maintenance Budget';
 
     protected static ?string $pluralModelLabel = 'Maintenance Budget & Realization';
-
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
 
     public static function form(Schema $schema): Schema
     {
